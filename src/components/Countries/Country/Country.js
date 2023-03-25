25ecddf29bf544135cee6154fdf2efc40893a659
Aditta Chakraborty <adittacse@gmail.com>
2023-03-26 05:18:08 +0600

    rest countries using react complete

--- src/components/Countries/Country/Country.js
+++ src/components/Countries/Country/Country.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "./Country.css";
 
 const Country = (props) => {
-    const {name, population, area, region} = props.country;
+    const {name, population, area, region, flags, startOfWeek} = props.country;
     return (
         <div className="country">
             <h2>{name.common}</h2>
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Region: {region}</small></p>
+            <img src={flags.png} alt=""/>
         </div>
     );
 };
